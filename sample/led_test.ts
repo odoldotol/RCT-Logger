5fe05abf3e9c103cad09917b729c5db95ba45769
odoldotol <odoldotol@gmail.com>
2025-01-23 15:56:11 +0900

    smaple: test onoff module by led

---
+++ sample/led_test.ts
@@ -0,0 +1,13 @@
+// GPIO18 에 연결된 LED 1초간 켜고 끄기
+
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(530, 'out');
+
+led.writeSync(1);
+setTimeout((_: unknown) => {
+  led.writeSync(0);
+}, 1000);
+
+process.on('SIGINT', _ => {
+  led.unexport();
+});
