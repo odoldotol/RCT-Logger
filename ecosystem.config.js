dcc3e343a0a686e19e3b6d6f4e67d3d86820df7d
odoldotol <odoldotol@gmail.com>
2025-02-22 04:17:05 +0900

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'RCTLogger',
+      script: 'dist/logger/main.js',
+      restart_delay: 500,
+      error_file: '/home/lygo/rct/rct_logger_error.log', // temp
+      out_file: '/home/lygo/rct/rct_logger_out.log', // temp
+      max_memory_restart: '200M',
+    }
+  ]
+};
