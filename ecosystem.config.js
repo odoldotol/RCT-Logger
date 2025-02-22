44fb7f702f4331ea13b5abfe9a205d08d0bd19d7
odoldotol <odoldotol@gmail.com>
2025-02-22 15:24:02 +0900

    doc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       restart_delay: 500,
       error_file: '/home/lygo/rct/rct_logger_error.log', // temp
       out_file: '/home/lygo/rct/rct_logger_out.log', // temp
-      max_memory_restart: '200M',
+      max_memory_restart: '300M',
     }
   ]
 };
