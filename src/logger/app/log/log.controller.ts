cae0c4b081c257a88362a3a75fdd9c3c13e6f0b2
odoldotol <odoldotol@gmail.com>
2025-02-27 04:26:03 +0900

    refac: logController

--- src/logger/app/log/log.controller.ts
+++ src/logger/app/log/log.controller.ts
@@ -1,23 +1,39 @@
+import { WriteStream } from "fs";
 import { B103ExtractedData } from "../../../common";
 import { LogService } from "./log.service";
+import { LogExcelService } from "./excel.service";
 
 
 export class LogController {
 
   constructor(
-    private readonly logService: LogService
+    private readonly logService: LogService,
+    private readonly logExcelService: LogExcelService
   ) {}
 
+  /**
+   * DB 가 분리 되면 비동기로 전환됨
+   */
   public log(dataBuffer: B103ExtractedData) {
     this.logService.log(dataBuffer);
   }
 
-  // public download(writeDir: string, downloadDate: DownloadDate) {}
+  /**
+   * Not implemented about rangeQuery
+   */
+  public async download(
+    writeStream: WriteStream,
+    // ranges: string[] // rangeQuery 를 파싱(줄바꿈으로 스플릿하고 각 요소는 10자리숫자, 10자리숫자-10자리숫자, 2자리숫자 이어야함)
+    _rangeQuery: string
+  ): Promise<void> {
+    const rangeArr: string[] = []; // temp
 
-  public fakeDownload(writeDir: string) {
-    return this.logService.fakeDownload(writeDir);
+    const segmentArr = await this.logService.getSegmentsByRanges(rangeArr);
+
+    return this.logExcelService.write(
+      writeStream,
+      segmentArr
+    );
   }
 
 }
-
-// type DownloadDate = string[];
